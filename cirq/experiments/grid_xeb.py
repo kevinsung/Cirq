b945e55f7b600dcce01b05608bfcb34e83209193
Kevin J. Sung <kevinjefferysung@gmail.com>
2020-02-21 10:35:21 -0800

    use multiprocessing

--- cirq/experiments/grid_xeb.py
+++ cirq/experiments/grid_xeb.py
@@ -5,6 +5,8 @@ import collections
 import dataclasses
 import datetime
 import itertools
+import multiprocessing
+from multiprocessing.managers import BaseManager, DictProxy
 import os
 
 import numpy as np
@@ -102,7 +104,7 @@ def collect_parallel_two_qubit_xeb_on_grid_data(
     # Generate and save all circuits
     max_cycles = max(cycles)
     circuits_ = collections.defaultdict(
-            list)  # type: Dict[GridInteractionLayer, List[cirq.Circuit]]
+        list)  # type: Dict[GridInteractionLayer, List[cirq.Circuit]]
     for layer in layers:
         for i in range(num_circuits):
             circuit = random_rotations_between_grid_interaction_layers_circuit(
@@ -115,9 +117,9 @@ def collect_parallel_two_qubit_xeb_on_grid_data(
                 seed=prng)
             circuits_[layer].append(circuit)
             params = ParallelXEBCircuitParameters(
-                    data_collection_id=data_collection_id,
-                    layer=layer,
-                    circuit_index=i)
+                data_collection_id=data_collection_id,
+                layer=layer,
+                circuit_index=i)
             fn = os.path.join(base_dir, params.fn)
             os.makedirs(os.path.dirname(fn), exist_ok=True)
             protocols.to_json(circuit, fn)
@@ -141,17 +143,23 @@ def collect_parallel_two_qubit_xeb_on_grid_data(
                     circuit_index=i)
                 fn = os.path.join(base_dir, params.fn)
                 os.makedirs(os.path.dirname(fn), exist_ok=True)
-                protocols.to_json(
-                    {
-                        'circuit': truncated_circuit,
-                        'trial_result': trial_result
-                    }, fn)
+                protocols.to_json(trial_result, fn)
 
     return data_collection_id
 
 
+class _FidelityEstimatorManager(BaseManager):
+    pass
+
+
+_FidelityEstimatorManager.register('defaultdict', collections.defaultdict,
+                                   DictProxy)
+
+
 def compute_parallel_two_qubit_xeb_on_grid_fidelities(
-        data_collection_id: str, base_dir: str = DEFAULT_BASE_DIR
+        data_collection_id: str,
+        num_processors: int = 1,
+        base_dir: str = DEFAULT_BASE_DIR
 ) -> Dict[Tuple['cirq.GridQubit', 'cirq.GridQubit'], CrossEntropyResult]:
 
     fn = os.path.join(base_dir, data_collection_id, 'metadata.json')
@@ -163,44 +171,104 @@ def compute_parallel_two_qubit_xeb_on_grid_fidelities(
     layers = metadata['layers']
 
     coupled_qubit_pairs = _coupled_qubit_pairs(qubits)
+    all_active_qubit_pairs = []
     xeb_results = {
     }  # type: Dict[Tuple[cirq.GridQubit, cirq.GridQubit], CrossEntropyResult]
 
+    manager = _FidelityEstimatorManager()
+    manager.start()
+    numerators = manager.defaultdict(float)
+    denominators = manager.defaultdict(float)
+    arguments = []
+
     for layer in layers:
-        print(f'Layer {layer}')
         active_qubit_pairs = [
             pair for pair in coupled_qubit_pairs if pair in layer
         ]
-        xeb_pairs = collections.defaultdict(list) \
-    # type: Dict[Tuple[cirq.GridQubit, cirq.GridQubit], List[CrossEntropyPair]]
-
-        for depth in cycles:
-            print(f'Depth {depth}')
-            circuits_and_trial_results = []  # type: List[CircuitAndTrialResult]
-            for i in range(num_circuits):
+        all_active_qubit_pairs.extend(active_qubit_pairs)
+        for i in range(num_circuits):
+            params = ParallelXEBCircuitParameters(
+                data_collection_id=data_collection_id,
+                layer=layer,
+                circuit_index=i)
+            fn = os.path.join(base_dir, params.fn)
+            os.makedirs(os.path.dirname(fn), exist_ok=True)
+            circuit = protocols.read_json(fn)
+            trial_results = []
+            for depth in cycles:
                 params = ParallelXEBTrialResultParameters(
                     data_collection_id=data_collection_id,
                     layer=layer,
                     depth=depth,
                     circuit_index=i)
                 fn = os.path.join(base_dir, params.fn)
-                data = protocols.read_json(fn)
-                circuit_and_trial_result = CircuitAndTrialResult(
-                    circuit=data['circuit'], trial_result=data['trial_result'])
-                circuits_and_trial_results.append(circuit_and_trial_result)
+                trial_result = protocols.read_json(fn)
+                trial_results.append(trial_result)
             for qubit_pair in active_qubit_pairs:
-                print(f'Pair {qubit_pair}')
-                fidelity = _get_xeb_fidelity(qubit_pair,
-                                             circuits_and_trial_results)
-                xeb_pairs[qubit_pair].append(CrossEntropyPair(depth, fidelity))
-        for qubit_pair, xeb_pair_list in xeb_pairs.items():
-            xeb_results[qubit_pair] = CrossEntropyResult(  # type: ignore
-                data=xeb_pair_list, repetitions=repetitions)
+                arguments.append((qubit_pair, qubits, circuit, cycles,
+                                  trial_results, numerators, denominators))
+
+    num_processors = min(num_processors, len(arguments))
+    with multiprocessing.Pool(num_processors) as pool:
+        _ = pool.starmap(_get_fidelity_estimator_components, arguments)
+
+    for qubit_pair in all_active_qubit_pairs:
+        data = []
+        for depth in cycles:
+            fidelity = (numerators[(qubit_pair, depth)] /
+                        denominators[(qubit_pair, depth)])
+            data.append(CrossEntropyPair(depth, fidelity))
+        xeb_results[qubit_pair] = CrossEntropyResult(data=data,
+                                                     repetitions=repetitions)
 
     fn = os.path.join(base_dir, data_collection_id, 'fidelities.json')
     protocols.to_json(list(xeb_results.items()), fn)
 
     return xeb_results
+
+
+def _get_fidelity_estimator_components(
+        qubit_pair: Tuple['cirq.GridQubit'],
+        all_qubits: Sequence['cirq.GridQubit'], circuit: 'cirq.Circuit',
+        cycles: Sequence[int], trial_results: Sequence['cirq.TrialResult'],
+        numerators: Dict[Tuple[Tuple['cirq.GridQubit', 'cirq.GriQubit'], int],
+                         List[float]],
+        denominators: Dict[Tuple[Tuple['cirq.GridQubit', 'cirq.GriQubit'], int],
+                           List[float]]) -> None:
+
+    a, b = qubit_pair
+    qubit_indices = [all_qubits.index(a), all_qubits.index(b)]
+    # Get the circuit restricted to this qubit pair
+    restricted_circuit = circuits.Circuit(
+        op for op in circuit.all_operations()
+        if not set(op.qubits).isdisjoint(qubit_pair))
+
+    simulator = sim.Simulator()
+    step_results = simulator.simulate_moment_steps(restricted_circuit,
+                                                   qubit_order=qubit_pair)
+    moment_index = 0
+
+    for depth, trial_result in zip(cycles, trial_results):
+        # Get the measurements of this qubit pair
+        restricted_measurements = trial_result.measurements['m'][:,
+                                                                 qubit_indices]
+        # Convert length-2 bitstrings to integers
+        restricted_measurements_ints = (2 * restricted_measurements[:, 0] +
+                                        restricted_measurements[:, 1])
+        # Compute the theoretical probabilities
+        while moment_index < 2 * depth:
+            step_result = next(step_results)
+            moment_index += 1
+        amplitudes = step_result.state_vector()
+        probabilities = np.abs(amplitudes)**2
+        # Compute the values needed for fidelity calculation
+        experimental_value = 4 * np.mean(
+            probabilities[restricted_measurements_ints])
+        theoretical_value = 4 * np.sum(probabilities**2)
+        numerator = (experimental_value - 1) * (theoretical_value - 1)
+        denominator = (theoretical_value - 1)**2
+        numerators[(qubit_pair, depth)] += numerator
+        denominators[(qubit_pair, depth)] += denominator
 
 
 def _get_xeb_fidelity(qubit_pair, circuits_and_trial_results) -> float:
--- cirq/experiments/grid_xeb_test.py
+++ cirq/experiments/grid_xeb_test.py
@@ -32,24 +32,24 @@ def test_estimate_parallel_two_qubit_xeb_fidelity_on_grid(tmpdir):
         np.testing.assert_allclose(fidelities, 1.0, atol=0.1)
 
     # With depolarizing probability e
-    cycles = [60, 80, 100]
+    cycles = [20, 40, 60]
     e = 0.01
     data_collection_id = collect_parallel_two_qubit_xeb_on_grid_data(
         sampler=cirq.DensityMatrixSimulator(noise=cirq.depolarize(e),
                                             seed=65009),
         qubits=qubits,
         two_qubit_gate=two_qubit_gate,
-        num_circuits=50,
-        repetitions=10_000,
+        num_circuits=20,
+        repetitions=1_000,
         cycles=cycles,
         seed=14947,
         base_dir=base_dir)
     results = compute_parallel_two_qubit_xeb_on_grid_fidelities(
-        data_collection_id, base_dir=base_dir)
+        data_collection_id, num_processors=32, base_dir=base_dir)
 
     assert len(results) == 4
     for result in results.values():
-        fidelities = [xeb_pair.xeb_fidelity for xeb_pair in result.data]
-        np.testing.assert_allclose(fidelities,
-                                   [(1 - e * 16 / 15)**(4 * c) for c in cycles],
-                                   atol=1e-3)
+        depolarizing_model = result.depolarizing_model()
+        cycle_pauli_error = (1 - depolarizing_model.decay_constant) * 15 / 16
+        np.testing.assert_allclose(1 - cycle_pauli_error, (1 - e)**4)
+        np.testing.assert_allclose(depolarizing_model.coefficient, 1.0)
